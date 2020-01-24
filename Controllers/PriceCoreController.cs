ecddea0c95ef91bbc22853e040d25a61d83dc50e
tshah <tshah@ivp.in>
2020-01-24 14:02:28 -0500

    Initial Commit

---
+++ Controllers/PriceCoreController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace samplepricing.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PriceCoreController : ControllerBase
+    {
+        private readonly ILogger<PriceCoreController> _logger;
+	
+        public PriceCoreController(ILogger<PriceCoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public PricingCore Get(int id)
+        {
+            var rng = new Random();
+	    PricingCore pricingCore = new PricingCore();
+	    pricingCore.Date = DateTime.Today ;
+	    pricingCore.Price = new Decimal(rng.NextDouble()*50+10);
+	    pricingCore.Id = id ;
+	    return pricingCore;
+        }
+    }
+}
---
+++ PricingCore.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace samplepricing
+{
+    public class PricingCore
+    {
+        public DateTime Date { get; set; }
+
+	public Decimal Price {get; set; }
+
+	public long Id {get; set; }
+    }
+}
