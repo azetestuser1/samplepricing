355f100411bb54b5325736ba9dd21e2bd91440e3
azetestuser01 <azetestuser01@outlook.com>
2020-01-27 23:53:25 -0500

    Added some comment

--- PricingCore.cs
+++ PricingCore.cs
@@ -13,6 +13,6 @@ namespace samplepricing
 	public string Currency {get; set; }
 	    
 	public string PriceSource {get; set; }
-	/*Added a comment more data on tagging for base tag*/
+	/*Added a comment more data on tagging for base tag for next verison*/
     }
 }
